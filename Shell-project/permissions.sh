13800f1cb98b5412568dc5af1d698f939225896b
Harshavardhan <reddyvardhan7890@gmail.com>
2026-06-20 19:06:55 +0530

    new files

--- Shell-project/permissions.sh
+++ Shell-project/permissions.sh
@@ -1,11 +1,10 @@
+
 #!/bin/bash
 
 echo "updating the permissions of the files"
 
 chmod -R 755 website
 
-chmod 644 orders/orders
-
-Chmod +x scripts/*.sh
+chmod +x *.sh
 
 echo "permissions updated successfully"
