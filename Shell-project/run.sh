54710e862d93ea0d48ae7736eb66c10b312f071e
Harshavardhan <reddyvardhan7890@gmail.com>
2026-06-20 20:06:03 +0530

    Update orders and nginx config

--- Shell-project/run.sh
+++ Shell-project/run.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-chmod +x launch-instance.sh
+#!/bin/bash
+set -e
+
+chmod +x *.sh
+
+./install-dependencies.sh
+
 ./launch-instance.sh
+
+./permissions.sh
+
+./deploy.sh
+
+echo "Pizza website deployed successfully!"
+
+./orders.sh Harsha2145@gmail.com
+
+echo "Pizza order placed successfully! Check your email for updates on your order status."
