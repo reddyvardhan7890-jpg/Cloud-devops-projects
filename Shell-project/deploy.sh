cf006be5ed833a4d6838f2887e7e9a2d126c802d
Harshavardhan <reddyvardhan7890@gmail.com>
2026-06-20 19:02:40 +0530

    new files

--- Shell-project/deploy.sh
+++ Shell-project/deploy.sh
@@ -2,15 +2,15 @@
 
 echo "removing default website"
 
-sudo rm-f /usr/share/niginx/html/*
+sudo rm -rf /usr/share/nginx/html/*
 
 echo "copy the html files"
 
-sudo cp -r website/* /usr/share/niginx/html/
+sudo cp -r website/* /usr/share/nginx/html/
 
 echo "changing the ownership of the files"
 
-sudo chmod -R 755 /usr/share/niginx/html
+sudo chmod -R 755 /usr/share/nginx/html
 
 sudo systemctl restart nginx
 
